87430437a055509d3b987d8b51117cbebfc43171
Vegard Bønes <vegard.bones@met.no>
2009-03-12 11:41:56 +0000

    Various updates to make this compile on both debian and ubuntu

--- debian_files/wdb.cron.d
+++ debian_files/wdb.cron.d
@@ -1,4 +1,5 @@
 SHELL=/bin/sh
 
 
-15 4 * * * su postgres -c "psql -d`/usr/bin/wdbConfiguration --database` -p`/usr/bin/wdbConfiguration --port` -c\"SELECT cleanupdb()\""
+36 9 * * * postgres psql -d`/usr/bin/wdbConfiguration --database` -p`/usr/bin/wdbConfiguration --port` -c"SELECT cleanupdb()"
+
