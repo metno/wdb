c2b769471e5c268c9463f39ddb4839c7cea3adbd
Michael Akinde <michael.akinde@met.no>
2009-05-19 10:04:41 +0000

    Added test to check if files are actually deleted on a cleanup

--- test/install/cleaningProgram/wdb.mk
+++ test/install/cleaningProgram/wdb.mk
@@ -3,39 +3,39 @@
 # WDB CleanDB Install Tests
 #-----------------------------------------------------------------------------
 
-CLEANDBTEST_SOURCES =		test/install/cleaningProgram/caseF001.in.test \
-				test/install/cleaningProgram/caseF002.in.test \
-				test/install/cleaningProgram/caseF003.in.test \
-				test/install/cleaningProgram/caseF004.in.test \
-				test/install/cleaningProgram/caseF005.in.test 
+CLEANDBTEST_SOURCES =	test/install/cleaningProgram/caseF001.in.test \
+						test/install/cleaningProgram/caseF002.in.test \
+						test/install/cleaningProgram/caseF003.in.test \
+						test/install/cleaningProgram/caseF004.in.test \
+						test/install/cleaningProgram/caseF005.in.test \
+						test/install/cleaningProgram/caseF006.in.test 
 
-CLEANDBTEST_SUPPORT =		test/install/cleaningProgram/tearDown.in.sh \
-				test/install/cleaningProgram/testEnvironment.in.sh
+CLEANDBTEST_SUPPORT =	test/install/cleaningProgram/tearDown.in.sh \
+						test/install/cleaningProgram/testEnvironment.in.sh
 
 
 INSTALLTESTS +=			cleanDbInstallTest.sh
 
 noinst_SCRIPTS +=		cleanDbInstallTest.sh \
-				$(CLEANDBTEST_SOURCES:.in.test=.test) \
-				$(CLEANDBTEST_SUPPORT:.in.sh=.sh)
+						$(CLEANDBTEST_SOURCES:.in.test=.test) \
+						$(CLEANDBTEST_SUPPORT:.in.sh=.sh)
 
 CLEANFILES +=			cleanDbInstallTest.sh \
-				$(CLEANDBTEST_SOURCES:.in.test=.test) \
-				$(CLEANDBTEST_SUPPORT:.in.sh=.sh)
+						$(CLEANDBTEST_SOURCES:.in.test=.test) \
+						$(CLEANDBTEST_SUPPORT:.in.sh=.sh)
 
 EXTRA_DIST +=			test/install/cleaningProgram/cleanDbInstallTest.in.sh \
-				$(CLEANDBTEST_SOURCES) \
-				$(CLEANDBTEST_SUPPORT) \
-				test/install/cleaningProgram/wdb.mk \
-				test/install/cleaningProgram/Makefile.am \
-				test/install/cleaningProgram/Makefile.in
+						$(CLEANDBTEST_SOURCES) \
+						$(CLEANDBTEST_SUPPORT) \
+						test/install/cleaningProgram/wdb.mk \
+						test/install/cleaningProgram/Makefile.am \
+						test/install/cleaningProgram/Makefile.in
 
 DISTCLEANFILES +=		test/install/cleaningProgram/Makefile
 
-cleanDbInstallTest.sh:		test/install/cleaningProgram/cleanDbInstallTest.in.sh
-				$(SED_SUBSTITUTION);\
-				chmod 754 $@
-
+cleanDbInstallTest.sh:	test/install/cleaningProgram/cleanDbInstallTest.in.sh
+						$(SED_SUBSTITUTION);\
+						chmod 754 $@
 
 
 
