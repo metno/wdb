466479f9dad43c783c3f7c19ae99119931091b45
Vegard Bønes <vegard.bones@met.no>
2009-09-02 11:40:47 +0000

    m4-macro for geos

--- src/callInterface/wdb.mk
+++ src/callInterface/wdb.mk
@@ -1,14 +1,14 @@
 lib_LTLIBRARIES +=		wdbCall.la
 wdbCall_la_SOURCES =
 wdbCall_la_LDFLAGS = 	-module $(LIBTOOL_VERSION_FLAG)
-wdbCall_la_CPPFLAGS = 	$(AM_CPPFLAGS) -I`pg_config --includedir-server` -I$(srcdir)/src/callInterface/
-wdbCall_la_LIBADD =		libwdbProj.a libwdbMath.a libwdbExcept.a -lgeos_c
+wdbCall_la_CPPFLAGS = 	$(AM_CPPFLAGS) -I`pg_config --includedir-server` -I$(srcdir)/src/callInterface/ $(geos_CFLAGS)
+wdbCall_la_LIBADD =		libwdbProj.a libwdbMath.a libwdbExcept.a $(geos_LIBS)
 
 wcidir = 				$(sqldir)/wci
 
 if HAS_CPPUNIT
 wdbUnitTest_LDADD += 	libwciCoreNoPostgres.la libwciTypesNoPostgres.la
 wdbUnitTest_LDADD += 	libwdbProj.a libwdbMath.a libwdbExcept.a
-wdbUnitTest_LDADD += 	-lgeos_c
+wdbUnitTest_LDADD += 	$(geos_LIBS)
 # HAS_CPPUNIT
 endif
