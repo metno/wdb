bd9401decf59e4541d0d43af30e9a946196cf0e0
Michael Akinde <michael.akinde@met.no>
2009-10-12 11:34:15 +0000

    Small fix to the clean up routines.

--- test/install/callInterface/tearDown.in.sh
+++ test/install/callInterface/tearDown.in.sh
@@ -26,4 +26,6 @@ psql `./wdbConfiguration --psqlArgs` -q <<EOF
 DELETE FROM __WDB_SCHEMA__.gridvalue WHERE dataproviderid >= 0 AND dataproviderid < 100;
 DELETE FROM __WDB_SCHEMA__.floatvalue WHERE dataproviderid >= 0 AND dataproviderid < 100;
 DELETE FROM __WDB_SCHEMA__.dataprovider WHERE dataproviderid IN ( SELECT dataproviderid FROM __WDB_SCHEMA__.dataprovidername WHERE dataprovidername LIKE 'installtest%' );
+DELETE FROM __WDB_SCHEMA__.placedefinition WHERE placeid IN ( SELECT placeid FROM __WDB_SCHEMA__.placename WHERE placename LIKE 'installtest%' );
+DELETE FROM __WDB_SCHEMA__.measure WHERE measurename LIKE 'installtest%';
 EOF
