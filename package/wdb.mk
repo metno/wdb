b4c04ea6dcc1f3879c07f87ce72154a400f00cdb
Michael Akinde <michael.akinde@met.no>
2011-02-25 11:43:21 +0000

    Updated debian building.

---
+++ package/wdb.mk
@@ -0,0 +1,9 @@
+
+#-----------------------------------------------------------------------------
+# WDB Package Scripts
+#-----------------------------------------------------------------------------
+
+EXTRA_DIST +=			package/compat \
+						package/control \
+						package copyright \
+						package/rules
