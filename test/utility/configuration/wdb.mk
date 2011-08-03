6683ed3ed67c7cbb15664f21a0b4f172f89282b9
Vegard Bønes <vegard.bones@met.no>
2011-08-03 15:34:02 +0200

    Wdb now compiles flawlessly without cppunit https://github.com/wdb/wdb/issues/14

--- test/utility/configuration/wdb.mk
+++ test/utility/configuration/wdb.mk
@@ -5,6 +5,16 @@
 # Library
 #-----------------------------------------------------------------------------
 
+EXTRA_DIST +=			test/utility/configuration/wdb.mk \
+						test/utility/configuration/Makefile.am \
+						test/utility/configuration/Makefile.in
+
+DISTCLEANFILES +=		test/utility/configuration/Makefile
+
+
+
+if HAS_CPPUNIT
+
 noinst_LTLIBRARIES += 		libwdbTest.la
 
 libwdbTest_la_SOURCES = test/utility/configuration/testConfiguration.cpp
@@ -14,16 +24,15 @@ pkginclude_HEADERS += 	test/utility/configuration/testConfiguration.h
 libwdbTest_la_CPPFLAGS = \
 						$(AM_CPPFLAGS) -fPIC
 
-EXTRA_DIST +=			test/utility/configuration/wdb.mk \
-						test/utility/configuration/Makefile.am \
-						test/utility/configuration/Makefile.in
-
-DISTCLEANFILES +=		test/utility/configuration/Makefile
 
 
 # Local Makefile Targets
 #-----------------------------------------------------------------------------
 
 test/utility/configuration/all: libwdbTest.la
+
+endif
+
+
 
 test/utility/configuration/clean: clean
