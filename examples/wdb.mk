7293287ec677c5b5a48ca315e751874b6e19ef83
Vegard Bønes <vegard.bones@met.no>
2011-10-10 12:25:26 +0200

    updating examples so distcheck works. Also, fix to make wdbConfiguration _really_ work when running examples

--- examples/wdb.mk
+++ examples/wdb.mk
@@ -10,7 +10,7 @@ test_examples: examples/setupExamples.sh examples wdbConfiguration
 	@echo '#!/bin/sh' > $@.tmp
 	@echo 'set -e' > $@.tmp
 	@echo "./examples/setupExamples.sh" >> $@.tmp
-	@for T in $(CPP_EXAMPLES); do echo "echo $$T && ./$$T \`./wdbConfiguration --pqxxArgs\` >> /dev/null" >> $@.tmp; done
+	@for T in $(CPP_EXAMPLES); do echo "echo $$T && ./$$T \"\`./wdbConfiguration --pqxxArgs\`\" >> /dev/null" >> $@.tmp; done
 	@for T in $(SQL_EXAMPLES); do echo "echo $$T && psql $(DATABASE_NAME) < $(top_srcdir)/$$T >> /dev/null" >> $@.tmp; done
 	@echo "psql `./wdbConfiguration --psqlArgs` -c \"SELECT wci.begin('cleantest',999,999,999); SELECT cleanupdb()\" >> /dev/null" >> $@.tmp
 	@mv $@.tmp $@
