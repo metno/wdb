89bcbbb5a744dab7daef104fc02ab7f1ea114a8d
Vegard Bønes <vegard.bones@met.no>
2011-10-14 12:28:36 +0200

    will no longer enter an infinite loop when database is empty

--- src/cleaningProgram/remove_low_dataversions/remove_low_dataversions.sql
+++ src/cleaningProgram/remove_low_dataversions/remove_low_dataversions.sql
@@ -4,12 +4,16 @@ CREATE OR REPLACE FUNCTION clean.clean_dataversions()
 RETURNS void AS
 $BODY$
 DECLARE
-	highest_dataversion int;
+	highest_dataversion int := 0;
 	current_version int := -1;
 	versions_to_delete int[];
 BEGIN
 	SELECT max(dataversion) INTO highest_dataversion FROM wci.browse(NULL::wci.browsedataversion);
 
+	IF highest_dataversion IS NULL THEN
+		RETURN;
+	END IF;
+	
 	LOOP
 		IF current_version < -highest_dataversion THEN
 			EXIT;
