42df806ef26b17fc4c3943f43686b3b294ece5de
Michael Akinde <michael.akinde@met.no>
2012-04-24 14:30:49 +0200

    Fixed makefiles for cleaning programs.

--- src/cleaningProgram/remove_low_dataversions/wdb.mk
+++ src/cleaningProgram/remove_low_dataversions/wdb.mk
@@ -1,6 +1,6 @@
 cleansql_DATA += src/cleaningProgram/remove_low_dataversions/remove_low_dataversions.sql
 
-pkglibexec_SCRIPTS += src/cleaningProgram/remove_low_dataversions/install_remove_low_dataversions.sh
+pkglib_SCRIPTS += src/cleaningProgram/remove_low_dataversions/install_remove_low_dataversions.sh
 
 src/cleaningProgram/remove_low_dataversions/install_remove_low_dataversions.sh: src/cleaningProgram/remove_low_dataversions/install_remove_low_dataversions.in.sh
 	mkdir -p `dirname $@`
