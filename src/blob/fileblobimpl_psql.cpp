72914fe07fc5f765908da58e563a061a1a7edb4e
Michael Akinde <michael.akinde@met.no>
2012-05-24 23:14:31 +0200

    Inserted config.h reference.

--- src/blob/fileblobimpl_psql.cpp
+++ src/blob/fileblobimpl_psql.cpp
@@ -25,7 +25,9 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  MA  02110-1301, USA
  */
-
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 #include "fileblobimpl_psql.h"
 #include "fileblobImpl.h"
 #include "PathGenerator.h"
