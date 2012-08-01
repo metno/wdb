5381bea7ce56fcb6e928b43ceec88093a02c875b
Vegard Bønes <vegard.bones@met.no>
2012-08-01 09:33:28 +0200

    corrected an example script

--- examples/wdb.mk
+++ examples/wdb.mk
@@ -12,7 +12,7 @@ test_examples: examples/setupExamples.sh examples wdbConfiguration
 	@echo "./examples/setupExamples.sh" >> $@.tmp
 	@for T in $(CPP_EXAMPLES); do echo "echo $$T && ./$$T \"\`./wdbConfiguration --pqxxArgs\`\" >> /dev/null" >> $@.tmp; done
 	@for T in $(SQL_EXAMPLES); do echo "echo $$T && psql $(DATABASE_NAME) < $(top_srcdir)/$$T >> /dev/null" >> $@.tmp; done
-	@echo "psql `./wdbConfiguration --psqlArgs` -c \"SELECT wci.begin('cleantest',999,999,999); SELECT cleanupdb()\" >> /dev/null" >> $@.tmp
+	@echo "psql $(DATABASE_NAME) -c \"SELECT wci.begin('cleantest',999,999,999); SELECT cleanupdb()\" >> /dev/null" >> $@.tmp
 	@mv $@.tmp $@
 	@chmod 774 $@
 
