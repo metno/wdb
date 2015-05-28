ca662bd1f1c4d8c00a2924abef6c69db388f35f5
Michael Akinde <michael.akinde@met.no>
2015-05-28 14:42:27 +0200

    Added AM_PROG_AR for compatibility with latest Autoconf

--- src/admin/operations/wdbDataKey.cpp
+++ src/admin/operations/wdbDataKey.cpp
@@ -9,7 +9,7 @@
     0313 OSLO
     NORWAY
     E-mail: wdb@met.no
-  
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -22,7 +22,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
     MA  02110-1301, USA
 */
 
